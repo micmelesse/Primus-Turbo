10f184a4557ea9c07df743019c9ed0b47685d167
xiaobochen-amd <xiaobo.chen@amd.com>
2025-06-23 13:01:19 +0800

    fix(gemm): ck fp8 gemm add meta func (#14)

--- csrc/pytorch/bindings_pytorch.cpp
+++ csrc/pytorch/bindings_pytorch.cpp
@@ -12,6 +12,10 @@ torch::Tensor gemm_fp8_blockwise(torch::Tensor &a, torch::Tensor &a_scales, torc
                                  torch::Tensor &b_scales, torch::Tensor &c, const bool transA,
                                  const bool transB, const int64_t block_size);
 
+torch::Tensor gemm_fp8_blockwise_meta(torch::Tensor &a, torch::Tensor &a_scales, torch::Tensor &b,
+                                      torch::Tensor &b_scales, torch::Tensor &c, const bool transA,
+                                      const bool transB, const int64_t block_size);
+
 TORCH_LIBRARY(primus_turbo_cpp_extension, m) {
     m.def("gemm(Tensor a, Tensor b) -> Tensor");
     m.def("gemm_fp8_blockwise("
@@ -29,7 +33,8 @@ TORCH_LIBRARY_IMPL(primus_turbo_cpp_extension, CUDA, m) {
 }
 
 TORCH_LIBRARY_IMPL(primus_turbo_cpp_extension, Meta, m) {
-    m.impl("gemm", gemm);
+    m.impl("gemm", gemm_meta);
+    m.impl("gemm_fp8_blockwise", gemm_fp8_blockwise_meta);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- csrc/pytorch/gemm/gemm_meta.cpp
+++ csrc/pytorch/gemm/gemm_meta.cpp
@@ -11,4 +11,12 @@ torch::Tensor gemm_meta(torch::Tensor a, torch::Tensor b) {
     return at::empty({m, n}, a.options().device(at::kMeta));
 }
 
+torch::Tensor gemm_fp8_blockwise_meta(torch::Tensor &a, torch::Tensor &a_scales, torch::Tensor &b,
+                                      torch::Tensor &b_scales, torch::Tensor &c, const bool transA,
+                                      const bool transB, const int64_t block_size) {
+    const int32_t m = transA ? a.size(1) : a.size(0);
+    const int32_t n = transB ? b.size(0) : b.size(1);
+    return at::empty({m, n}, c.options().device(at::kMeta));
+}
+
 } // namespace primus_turbo::pytorch
