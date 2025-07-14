821b5d93e0dd398404812e61e5eb12b1e38e4ed1
xiaobochen-amd <xiaobo.chen@amd.com>
2025-07-14 05:30:28 +0000

    add HIPExtension & refactor code Co-authored-by: zhenhuang12 <Zhen.Huang@amd.com>

--- primus_turbo/pytorch/__init__.py
+++ primus_turbo/pytorch/__init__.py
@@ -1,8 +1,9 @@
 import torch
 
+import primus_turbo.pytorch._C
+import primus_turbo.pytorch.modules
+import primus_turbo.pytorch.ops
 from primus_turbo.pytorch.core import float8
-
-from . import _C, modules, ops
 
 float8_e4m3 = float8.float8_e4m3
 float8_e5m2 = float8.float8_e5m2
