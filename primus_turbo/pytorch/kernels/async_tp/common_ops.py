ff2c759180bb49cd3fa2f848b6bb282322acf6f0
zhenhuang12 <Zhen.Huang@amd.com>
2025-08-15 19:51:03 +0800

    fix(async_tp): make memory order safe by atomic release-store, acquire-load (#63)

--- primus_turbo/pytorch/kernels/async_tp/common_ops.py
+++ primus_turbo/pytorch/kernels/async_tp/common_ops.py
@@ -23,11 +23,20 @@ def hip_check(call_result):
 
 
 @triton.jit
-def batch_wait_eq_sys(rank, barrier_ptr, n_elements: tl.constexpr):
+def wait_signal_acq_sys(rank, barrier_ptr, n_elements: tl.constexpr):
     barrier_ptr = barrier_ptr.to(tl.pointer_type(tl.int32))
     for i in range(n_elements):
-        while i != rank and tl.atomic_cas(barrier_ptr + i, 1, 0, scope="sys", sem="acq_rel") != 1:
+        while i != rank and tl.atomic_cas(barrier_ptr + i, 1, 0, scope="sys", sem="acquire") != 1:
             pass
+
+    tl.debug_barrier()
+
+
+@triton.jit
+def put_signal_rel_sys(barrier_ptr):
+    barrier_ptr = barrier_ptr.to(tl.pointer_type(tl.int32))
+    while tl.atomic_cas(barrier_ptr, 0, 1, scope="sys", sem="release") != 0:
+        pass
 
     tl.debug_barrier()
 
