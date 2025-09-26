5fb2c17d54355ed94d253a64842568778686123d
kyle-256 <xianzhao@amd.com>
2025-09-26 21:18:58 +0800

    opt: update grouped gemm config on gfx950 & improve perf (#118)

--- csrc/kernels/grouped_gemm/ck_grouped_gemm_kernel.h
+++ csrc/kernels/grouped_gemm/ck_grouped_gemm_kernel.h
@@ -316,19 +316,42 @@ get_ck_grouped_gemm_instance_gfx950(const ck_tile::index_t group_num, const ck_t
     if (get_current_arch() != GPUArch::GFX950) {
         PRIMUS_TURBO_ERROR("Currently Arch != gfx950");
     }
-
     if constexpr (std::is_same_v<ADataType, ck_tile::half_t> ||
                   std::is_same_v<ADataType, ck_tile::bfloat16_t>) {
-        using TileConfig = CKGroupedGemmTileCfg_128x128x64_32x32x16_2x2x1_padding;
-        using Runner = CKGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout, BLayout,
-                                            CLayout, TileConfig, AccDataType>;
-        runner = std::make_unique<Runner>();
+        if (n % 256 == 0) {
+            using TileConfig = CKGroupedGemmTileCfg_256x256x64_32x32x16_2x2x1;
+            using Runner = CKGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout, BLayout,
+                                               CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        } else if (n % 128 == 0) {
+            using TileConfig = CKGroupedGemmTileCfg_256x128x64_32x32x16_2x2x1;
+            using Runner = CKGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout, BLayout,
+                                               CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        } else {
+            using TileConfig = CKGroupedGemmTileCfg_256x128x64_32x32x16_2x2x1_padding;
+            using Runner = CKGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout, BLayout,
+                                               CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        }
     } else if constexpr (std::is_same_v<ADataType, ck_tile::bf8_t> ||
                          std::is_same_v<ADataType, ck_tile::fp8_t>) {
-        using TileConfig = CKGroupedGemmTileCfg_128x128x128_32x32x64_2x2x1;
-        using Runner = CKQuantGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout, BLayout,
-                                               CLayout, TileConfig, AccDataType>;
-        runner = std::make_unique<Runner>();
+        if (n % 256 == 0) {
+            using TileConfig = CKGroupedGemmTileCfg_256x256x128_16x16x128_2x2x1;
+            using Runner     = CKQuantGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout,
+                                                        BLayout, CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        } else if (n % 128 == 0) {
+            using TileConfig = CKGroupedGemmTileCfg_256x256x128_16x16x128_2x2x1_padding;
+            using Runner     = CKQuantGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout,
+                                                        BLayout, CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        } else {
+            using TileConfig = CKGroupedGemmTileCfg_128x128x128_32x32x64_2x2x1;
+            using Runner     = CKQuantGroupedGemmRunner<GPUArch::GFX950, ADataType, BDataType, CDataType, ALayout,
+                                                        BLayout, CLayout, TileConfig, AccDataType>;
+            runner = std::make_unique<Runner>();
+        }
     } else {
         PRIMUS_TURBO_ERROR("Grouped Gemm only support fp16/bf16/fp8/bf8");
     }
--- csrc/kernels/grouped_gemm/ck_grouped_gemm_kernel_config.h
+++ csrc/kernels/grouped_gemm/ck_grouped_gemm_kernel_config.h
@@ -1,14 +1,10 @@
 // Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
 //
 // See LICENSE for license information.
-
 #pragma once
-
 #include "ck_tile/core.hpp"
-
 namespace primus_turbo {
 // clang-format off
-
 template<
     ck_tile::index_t M_Tile_,
     ck_tile::index_t N_Tile_,
@@ -26,45 +22,33 @@ struct CKGroupedGemmTileConfig {
     static constexpr ck_tile::index_t M_Tile = M_Tile_;
     static constexpr ck_tile::index_t N_Tile = N_Tile_;
     static constexpr ck_tile::index_t K_Tile = K_Tile_;
-
     static constexpr ck_tile::index_t M_Warp = M_Warp_;
     static constexpr ck_tile::index_t N_Warp = N_Warp_;
     static constexpr ck_tile::index_t K_Warp = K_Warp_;
-
     static constexpr ck_tile::index_t M_Warp_Tile = M_Warp_Tile_;
     static constexpr ck_tile::index_t N_Warp_Tile = N_Warp_Tile_;
     static constexpr ck_tile::index_t K_Warp_Tile = K_Warp_Tile_;
-
     static constexpr bool DoubleSmemBuffer = DoubleSmemBuffer_;
-
     static constexpr bool kPadM = false;
     static constexpr bool kPadN = kPadN_;
     static constexpr bool kPadK = false;
-
     // static constexpr int              kBlockPerCu            = 1;
     static constexpr ck_tile::index_t TileParitionerGroupNum = 8;
     static constexpr ck_tile::index_t TileParitionerM01      = 4;
 };
-
 
 // ****** GFX942/GFX950 Tile Config Specialization ******
 // FP16
 using CKGroupedGemmTileCfg_256x256x64_32x32x16_2x2x1 = CKGroupedGemmTileConfig<
     256, 256, 64, 32, 32, 16, 2, 2, 1, false, false
 >;
-
 using CKGroupedGemmTileCfg_256x128x64_32x32x16_2x2x1 = CKGroupedGemmTileConfig<
     256, 128, 64, 32, 32, 16, 2, 2, 1, false, false
 >;
-
 using CKGroupedGemmTileCfg_256x128x64_32x32x16_2x2x1_padding = CKGroupedGemmTileConfig<
     256, 128, 64, 32, 32, 16, 2, 2, 1, false, true
 >;
-using CKGroupedGemmTileCfg_128x128x64_32x32x16_2x2x1_padding = CKGroupedGemmTileConfig<
-    128, 128, 64, 32, 32, 16, 2, 2, 1, false, true
->;
 // ***********************************************
-
 // ****** GFX942 Tile Config Specialization ******
 // FP8
 using CKGroupedGemmTileCfg_256x256x128_32x32x32_2x2x1 = CKGroupedGemmTileConfig<
@@ -77,16 +61,17 @@ using CKGroupedGemmTileCfg_256x128x128_32x32x32_2x2x1_padding = CKGroupedGemmTil
     256, 128, 128, 32, 32, 32, 2, 2, 1, false, true
 >;
 // ***********************************************
-
 // ****** GFX950 Tile Config Specialization ******
 // FP8
-using CKGroupedGemmTileCfg_128x128x128_32x32x64_2x2x1 = CKGroupedGemmTileConfig<
-    128, 128, 128, 32, 32, 64, 2, 2, 1, false, false
+using CKGroupedGemmTileCfg_256x256x128_16x16x128_2x2x1 = CKGroupedGemmTileConfig<
+    256, 256, 128, 16, 16, 128, 2, 2, 1, false, false
 >;
 using CKGroupedGemmTileCfg_256x256x128_16x16x128_2x2x1_padding = CKGroupedGemmTileConfig<
     256, 256, 128, 16, 16, 128, 2, 2, 1, false, true
 >;
+using CKGroupedGemmTileCfg_128x128x128_32x32x64_2x2x1 = CKGroupedGemmTileConfig<
+    128, 128, 128, 32, 32, 64, 2, 2, 1, false, false
+>;
 // ***********************************************
-
 // clang-format on
 } // namespace primus_turbo
