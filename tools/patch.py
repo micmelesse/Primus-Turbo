de6aa038a141e7b8b5064131196383b733fb6115
zhenhuang12 <Zhen.Huang@amd.com>
2025-11-04 13:55:00 +0800

    chore: skip patch torch when version >=2.8.0 (#144)

--- tools/patch.py
+++ tools/patch.py
@@ -5,38 +5,42 @@ from typing import Optional
 
 def patch_torch_extension():
     import torch
+    from torch.torch_version import TorchVersion
     from torch.utils import cpp_extension
 
-    # copy from torchv2.8.0-rc8
-    def _get_rocm_arch_flags(cflags: Optional[list[str]] = None) -> list[str]:
-        # If cflags is given, there may already be user-provided arch flags in it
-        # (from `extra_compile_args`). If user also specified -fgpu-rdc or -fno-gpu-rdc, we
-        # assume they know what they're doing. Otherwise, we force -fno-gpu-rdc default.
-        has_gpu_rdc_flag = False
-        if cflags is not None:
-            has_custom_flags = False
-            for flag in cflags:
-                if "amdgpu-target" in flag or "offload-arch" in flag:
-                    has_custom_flags = True
-                elif "gpu-rdc" in flag:
-                    has_gpu_rdc_flag = True
-            if has_custom_flags:
-                return [] if has_gpu_rdc_flag else ["-fno-gpu-rdc"]
-        # Use same defaults as used for building PyTorch
-        # Allow env var to override, just like during initial cmake build.
-        _archs = os.environ.get("PYTORCH_ROCM_ARCH", None)
-        if not _archs:
-            archFlags = torch._C._cuda_getArchFlags()
-            if archFlags:
-                archs = archFlags.split()
+    if TorchVersion(torch.__version__) < TorchVersion("2.8.0"):
+        warnings.warn(
+            f"Found Pytorch version {torch.__version__} < 2.8.0, patch _get_rocm_arch_flags to support -fgpu-rdc flag."
+        )
+
+        # copy from torchv2.8.0
+        def _get_rocm_arch_flags(cflags: Optional[list[str]] = None) -> list[str]:
+            # If cflags is given, there may already be user-provided arch flags in it
+            # (from `extra_compile_args`). If user also specified -fgpu-rdc or -fno-gpu-rdc, we
+            # assume they know what they're doing. Otherwise, we force -fno-gpu-rdc default.
+            has_gpu_rdc_flag = False
+            if cflags is not None:
+                has_custom_flags = False
+                for flag in cflags:
+                    if "amdgpu-target" in flag or "offload-arch" in flag:
+                        has_custom_flags = True
+                    elif "gpu-rdc" in flag:
+                        has_gpu_rdc_flag = True
+                if has_custom_flags:
+                    return [] if has_gpu_rdc_flag else ["-fno-gpu-rdc"]
+            # Use same defaults as used for building PyTorch
+            # Allow env var to override, just like during initial cmake build.
+            _archs = os.environ.get("PYTORCH_ROCM_ARCH", None)
+            if not _archs:
+                archFlags = torch._C._cuda_getArchFlags()
+                if archFlags:
+                    archs = archFlags.split()
+                else:
+                    archs = []
             else:
-                archs = []
-        else:
-            archs = _archs.replace(" ", ";").split(";")
-        flags = [f"--offload-arch={arch}" for arch in archs]
-        flags += [] if has_gpu_rdc_flag else ["-fno-gpu-rdc"]
-        return flags
+                archs = _archs.replace(" ", ";").split(";")
+            flags = [f"--offload-arch={arch}" for arch in archs]
+            flags += [] if has_gpu_rdc_flag else ["-fno-gpu-rdc"]
+            return flags
 
-    cpp_extension._get_rocm_arch_flags = _get_rocm_arch_flags
-
-    warnings.warn("Patch torch.utils.cpp_extension._get_rocm_arch_flags to support -fgpu-rdc flag!")
+        cpp_extension._get_rocm_arch_flags = _get_rocm_arch_flags
